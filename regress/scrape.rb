b966d795e9ab5eecfefbaba0a6476e3b18f4d9da
warmer <ken.kinion@gmail.com>
2015-12-26 16:53:06 -0500

    Removes HTTP GET functionality; simplifies page scraping utility

---
+++ regress/scrape.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+text = <<-HERE
+  <a href="foo.com">Foo!</a>
+  <a href='fizz.buzz'>
+    Fizz!
+  </a><a href=bizz.bang
+  >Bizz</a>
+HERE
+
+file = '/tmp/mm_scrape_test.txt'
+File.delete(file) if File.exist?(file)
+File.write(file, text)
+
+puts `../bin/scrape.rb #{file} "<a\\s+[^>]*href=['\\"]?(?<href>[^'\\"\\s]+)['\\"]?[^>]*>\\s*(?<name>[^<]*?)\\s*<\\/a>"`
